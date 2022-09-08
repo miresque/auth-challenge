6d63d323d0fab76193127354e91140d4f710ac4b
mirkan-k <mirkilinc@gmail.com>
2022-09-08 11:29:11 +0100

    feat: add login function and generate token on valid login

--- src/server/controllers/user.js
+++ src/server/controllers/user.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-const jwtSecret = 'mysecret';
+const jwtSecret = process.env.JWT_SECRET;
 
 const register = async (req, res) => {
     const { username, password } = req.body;
@@ -24,21 +24,25 @@ const register = async (req, res) => {
 const login = async (req, res) => {
     const { username, password } = req.body;
 
-    const foundUser = null;
+    const foundUser = await prisma.user.findFirst({
+        where: {
+            username
+        }
+    });
 
     if (!foundUser) {
         return res.status(401).json({ error: 'Invalid username or password.' });
     }
 
-    const passwordsMatch = false;
+    const passwordsMatch = await bcrypt.compare(password, foundUser.password);
 
     if (!passwordsMatch) {
         return res.status(401).json({ error: 'Invalid username or password.' });
     }
 
-    const token = null;
+    const token = jwt.sign({ username }, jwtSecret);
 
-    res.json({ data: token });
+    res.status(201).json({ token });
 };
 
 module.exports = {
